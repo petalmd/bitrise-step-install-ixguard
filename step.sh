aea83b06f9a08abe8ecab3f60ec206b4dd0b25fd
Nicholas Pinney <npinney@petalmd.com>
2024-11-12 16:38:35 -0500

    final

--- step.sh
+++ step.sh
@@ -4,6 +4,11 @@ set -e
 # Desired version of iXGuard
 REQUIRED_VERSION=${version}
 
+if [ -n "$REQUIRED_VERSION" ]; then
+    REQUIRED_VERSION="4.12.6"
+    echo "No version set. Selecting default version 4.12.6"
+fi
+
 if ! command -v ixguard >/dev/null 2>&1 || [[ "$(ixguard --version)" != *"$REQUIRED_VERSION"* ]]; then
     echo "iXGuard not found or incorrect version. Installing version $REQUIRED_VERSION..."
 
