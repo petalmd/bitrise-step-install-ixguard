b63ad36475e439d7320564a57e60d590b2030a74
Nicholas Pinney <npinney@petalmd.com>
2024-11-25 10:23:35 -0500

    Fixed conditionals

--- step.sh
+++ step.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
-set -ex
+set -e
 
 # Desired version of iXGuard
 REQUIRED_VERSION=${version}
 IXGUARD_SSH_KEY_FILE_URL=${ssh_key}
 
-# Download and install the ssh key for Guardsquare access
+# Download and install the ssh key for Guardsquare access if variable is truthy
 if [ -n "$IXGUARD_SSH_KEY_FILE_URL" ]; then
-    echo "SSH key for iXGuard access is missing."
-    exit 1
-else
     curl $IXGUARD_SSH_KEY_FILE_URL -o "ssh_private_key"
 
     eval "$(ssh-agent -s)"
     ssh-add --apple-use-keychain "ssh_private_key"
+else
+    echo "SSH key for iXGuard access is missing."
+    exit 1
 fi
 
 # Check if desired version of ixguard, otherwise use default
-if [ -n "$REQUIRED_VERSION" ]; then
-    REQUIRED_VERSION="4.12.6"
+if [ -z "$REQUIRED_VERSION" ]; then
     echo "No version set. Selecting default version 4.12.6"
+    REQUIRED_VERSION="4.12.6"
 fi
 
 if ! command -v ixguard >/dev/null 2>&1 || [[ "$(ixguard --version)" != *"$REQUIRED_VERSION"* ]]; then
